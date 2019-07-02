ad48b6dad6b2e1c13e5bd78b2816438abbbf3b4c
Richard Brice <bricer@wsdot.wa.gov>
2019-07-02 07:29:55 -0700

    test

--- BEToolboxExtensions/docs/conf.py
+++ BEToolboxExtensions/docs/conf.py
@@ -26,6 +26,7 @@ import os
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
   from subprocess import call 
+  print('running doxygen')
   call('doxygen')
 
 # -- General configuration ------------------------------------------------
