6db8c49aa9c35bdd8e6bd6352b94052d5331f4c5
boutilr <115045413+boutilr@users.noreply.github.com>
2024-06-20 16:49:30 -0700

    report clean up

--- Bearing/BearingChapterBuilder.cpp
+++ Bearing/BearingChapterBuilder.cpp
@@ -1503,7 +1503,7 @@ void ReportBearingSpecificationCheckB(rptChapter* pChapter, rptParagraph* pPara,
 		*pPara << _T(") ") << symbol(TIMES) << _T(" S / 3 / (n + ") << Sub2(symbol(mu), _T("tc allow")) << _T(") / (") << Sub2(symbol(sigma), _T("st"));
 		*pPara << _T(" + 1.75") << Sub2(symbol(sigma), _T("cy")) << _T(") ") << symbol(TIMES) << Sub2(_T(" E"), _T("B")) << _T(" = (");
 		*pPara << static_rotation << _T(" + 1.75 ") << symbol(TIMES) << _T(" ") << cyclic_rotation << _T(") ") << symbol(TIMES) << _T(" ") << s << _T(" / 3 / (");
-		*pPara << n << _T(" + ") << n_multiplier << _T(") / (") << stress.SetValue(static_stress) << _T(" + 1.75 ") << stress.SetValue(cyclic_stress);
+		*pPara << n << _T(" + ") << n_multiplier << _T(") / (") << stress.SetValue(static_stress) << _T(" + 1.75 ") << symbol(TIMES) << stress.SetValue(cyclic_stress);
 		*pPara << _T(") ") << symbol(TIMES) << _T(" ") << E.SetValue(EcB) << _T(" = ") << restraint_system_calc;
 		*pPara << _T(" < 1 ") << rptNewLine;
 		*pPara << symbol(RIGHT_SINGLE_ARROW) << color(Green) << _T("NO RESTRAINT SYSTEM REQUIRED") << color(Green);
