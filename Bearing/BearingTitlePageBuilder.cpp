0a6ba264b5dc46ce0ba09397a98060b58469ca29
Ron Boutilier <115045413+boutilr@users.noreply.github.com>
2025-05-29 17:00:20 -0700

    adds page break (Mantis 1588)

--- Bearing/BearingTitlePageBuilder.cpp
+++ Bearing/BearingTitlePageBuilder.cpp
@@ -94,7 +94,7 @@ rptChapter* CBearingTitlePageBuilder::Build(const std::shared_ptr<const WBFL::Re
 
    //*pPara << rptRcImage(std::_tstring(rptStyleManager::GetImagePath()) + _T("Bearing.PNG"));
 
-   //(*pPara) << rptNewPage;
+   (*pPara) << rptNewPage;
  
    return pTitlePage;
 }
