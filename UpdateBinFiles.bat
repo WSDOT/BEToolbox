f720ec41b18fc1b8f859f28334b9baf98d636264
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2025-12-10 12:54:20 -0800

    Updated version numbers for 9.0.0.1 release

--- UpdateBinFiles.bat
+++ UpdateBinFiles.bat
@@ -5,6 +5,9 @@ SET REGFREECOM=%ARPDIR%\BridgeLink\RegFreeCOM
 
 
 xcopy /Y /d %REGFREECOM%\x64\Release\BEToolbox.dll		%BINTARGET%\x64\
+xcopy /Y /d %REGFREECOM%\x64\Release\Application.Manifest.BEToolbox		%BINTARGET%\x64\
+xcopy /Y /d %REGFREECOM%\x64\Release\BEToolboxExtensions.Manifest.PGSuper		%BINTARGET%\x64\
+
 xcopy /Y /d %REGFREECOM%\x64\Release\BEToolboxXML.dll		%BINTARGET%\x64\
 xcopy /Y /d Images\*.png					%BINTARGET%\Images\
 xcopy /Y /d USGS\2014\*.bin                                     %BINTARGET%\
